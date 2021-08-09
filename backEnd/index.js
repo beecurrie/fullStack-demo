51e4ddab82cd9f4b8c41982311e7d273afdfe094
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2021-08-09 23:52:26 +0000

    initial commit

---
+++ backEnd/index.js
@@ -0,0 +1,12 @@
+const express =require('express');
+const app = express();
+
+const port = 3000;
+
+
+// Get Method to access
+
+
+// listen to port
+
+app.listen(port, ()=>console.log(`My fullstack application is listening on port ${port}`))
